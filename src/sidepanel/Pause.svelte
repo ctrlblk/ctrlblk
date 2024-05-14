75cd832110a01f58a2e95372e345911d75489b2f
Winsley von Spee <winsley@ctrlblk.com>
2024-05-14 17:41:53 +0200

    Changed Pause component flow, now exception & reload first
    The previous flow didn't take into account whether adding an exception would help with the problem or not and did suggest that adding & reloading an exception would always fix the problem which is not always the case.
    
    The new flow changes the order, exception adding and reloading takes place first, following feedback from the user the exception is now removed again in case it didn't fix the problem.

--- src/sidepanel/Pause.svelte
+++ src/sidepanel/Pause.svelte
@@ -11,9 +11,6 @@
         PauseSolid,
         CheckOutline,
         CloseOutline,
-        RefreshOutline,
-        ThumbsUpOutline,
-        ThumbsDownOutline,
     } from 'flowbite-svelte-icons';
 
     import { onMount } from 'svelte';
@@ -37,54 +34,10 @@
     let currentTabHostname = "";
     let currentTabExempt = false;
 
-    // Available states:
-    // - PlayPause
-    // - PreviewReport
-    // - SendReport
-    // - Reload
-    // - SendingReport
-    let defaultState = "PlayPause";
-    let state = defaultState;
+    let currentState = "PlayPause";
+    let currentTabId;
 
-    // Used to keep track of whether exempt status needs to be updated or not
-    // in case exceptions where manipulated outside of this component and
-    // reload functionality is still needed/offered i.e. offer reload when
-    // remocing an exception from the Exception component
-    // Available intents:
-    // - Play
-    // - Pause
-    let defaultIntent = "";
-    let intent = defaultIntent;
-
-    // Map to keep track of state and intent per tab
-    let tabState = new Map();
-
-
-    let tableData = [];
     let adReport = {};
-
-    async function changeExempt() {
-        if (intent === "Play") {
-            exceptions.update(value => value.filter(e => e !== currentTabHostname));
-        } else if (intent === "Pause") {
-            if (!$exceptions.includes(currentTabHostname)) {
-                exceptions.update(value => [...value, currentTabHostname]);
-            }
-        }
-        intent = defaultIntent;
-    }
-
-    function flattenObjectTree(obj, output, parents = []) {
-        for (let [key, value] of Object.entries(obj)) {
-            if (typeof value === "object" && !Array.isArray(value)) {
-                flattenObjectTree(value, output, parents.concat([key]));
-            } else if (Array.isArray(value)) {
-                output.push([parents.concat([key]).join('.'), value.join(', ')]);
-            } else {
-                output.push([parents.concat([key]).join('.'), value]);
-            }
-        }
-    }
 
     async function createAdReport() {
         let image = await browser.tabs.captureVisibleTab();
@@ -92,84 +45,98 @@
 
         let clonedData = JSON.parse(JSON.stringify(adReport.data));
         clonedData.screenshot = clonedData.screenshot.split(",")[0] + "...";
-        flattenObjectTree(clonedData, tableData);
-
-        // XXX: This is a hack to trigger svelte reactiveness
-        // to get the table to update because updating the tableData
-        // in flattenObjectTree doesn't trigger a re-render
-        tableData = tableData;
     }
 
-    async function clickPlayPause() {
-        // Advance state but don't change the exempt status yet
-        if (currentTabExempt) {
-            state = "Reload";
-            intent = "Play";
-        } else {
-            // Only offer to send a report if there are ad reports for the current tab
-            let adReportsForPage = await getAdReportsByDomains([currentTabHostname]);
+    async function clickPause() {
+        // Create ad report for future use
+        // Important: need to wait otherwhise report will include exception added below
+        // That wasn't present previously
+        await createAdReport();
 
-            if (adReportsForPage.has(currentTabHostname)) {
-                state = "Reload";
-            } else {
-                state = "SendReport";
-            }
-            intent = "Pause";
+        // Add exception
+        if (!$exceptions.includes(currentTabHostname)) {
+            exceptions.update(value => [...value, currentTabHostname]);
+        }
+
+        // Reload page
+        let [currentTab] = await browser.tabs.query({active: true, currentWindow: true});
+        browser.tabs.reload(currentTab.id);
+
+        currentState = "Ask";
+    }
+
+    async function clickPlay() {
+        // Remove exception
+        exceptions.update(value => value.filter(e => e !== currentTabHostname));
+
+        // Reload page
+        let [currentTab] = await browser.tabs.query({active: true, currentWindow: true});
+        browser.tabs.reload(currentTab.id);
+
+        currentState = "PlayPause";
+    }
+
+    async function clickItHelped() {
+
+        adReport.data.functional = {
+            pauseHelped: true,
+        }
+
+        // Only offer to send a report if there are no ad reports for the current tab
+        let adReportsForPage = await getAdReportsByDomains([currentTabHostname]);
+
+        if (adReportsForPage.has(currentTabHostname)) {
+            currentState = "PlayPause";
+        } else {
+            currentState = "PreviewReport|ItHelped";
         }
     }
 
-    function clickPreview() {
-        createAdReport();
-        state = "PreviewReport";
-    }
-    function clickNoAdReport() {
-        state = "Reload";
+    async function clickDidntHelp() {
+        adReport.data.functional = {
+            pauseHelped: false,
+        }
+
+        // Remove exception
+        exceptions.update(value => value.filter(e => e !== currentTabHostname));
+
+        // Reload page
+        let [currentTab] = await browser.tabs.query({active: true, currentWindow: true});
+        browser.tabs.reload(currentTab.id);
+
+        // Only offer to send a report if there are no ad reports for the current tab
+        let adReportsForPage = await getAdReportsByDomains([currentTabHostname]);
+
+        if (adReportsForPage.has(currentTabHostname)) {
+            currentState = "PlayPause";
+        } else {
+            currentState = "PreviewReport|DidntHelp";
+        }
     }
 
     async function clickSend() {
-        state = "SendingReport";
+        currentState = "SendingReport";
 
         adReport.uuid = await uploadAdReport(adReport.data);
 
-        state = "Reload";
-    }
-    function clickNoSend() {
-        state = "Reload";
+        currentState = "PlayPause";
     }
 
-    async function clickReload() {
-        await changeExempt();
-        let [currentTab] = await browser.tabs.query({active: true, currentWindow: true});
-        browser.tabs.reload(currentTab.id);
-        state = "PlayPause";
+    async function clickNoSend() {
+        currentState = "PlayPause";
     }
 
-    async function clickNoReload() {
-        await changeExempt();
-        state = "PlayPause";
-    }
 
-    let previousExceptions;
     function exceptionsChangedHandler(value) {
         // abort if exceptions hasn't been inititialized yet
         if (value === undefined) {
             return;
         }
 
-        let prevExempt = currentTabExempt;
         currentTabExempt = value.includes(currentTabHostname);
-
-        // Only update state in case of a change
-        // and not as part of inialization
-       if (previousExceptions !== undefined &&
-                prevExempt !== currentTabExempt) {
-            state = "Reload";
-        }
-
-        // Remember the previous exceptions for the above check
-        previousExceptions = $exceptions;
     }
 
+    let previousTabHostname;
     let previousTabId;
     async function tabChangedHandler(tabId) {
         // update currentTabHostname
@@ -177,9 +144,9 @@
         try {
             tabInfo = await browser.tabs.get(tabId);
 
-            // Abort if tab doesn't belong to the current window
+            // Abort if the tab isn't the selected tab or belongs to a different window
             let currentWindow = await browser.windows.getCurrent();
-            if (tabInfo.windowId !== currentWindow.id) {
+            if (!tabInfo.selected || tabInfo.windowId !== currentWindow.id) {
                 return;
             }
 
@@ -195,6 +162,16 @@
             currentTabHostname = "";
         }
 
+        // Update currentTabExempt & currentState if url has changed
+        if (previousTabId === undefined) {
+            currentTabExempt = $exceptions.includes(currentTabHostname);
+        } else {
+            if (previousTabHostname !== currentTabHostname) {
+                currentTabExempt = $exceptions.includes(currentTabHostname);
+                currentState = "PlayPause";
+            }
+        }
+
         // disable on internal pages like chrome:// and about://
         disabled = false;
         for (let prefix of ["chrome://" , "about://"]) {
@@ -204,123 +181,101 @@
             }
         }
 
-        // update whether currentTabHostname is exempt or not
-        if ($exceptions !== undefined) {
-            currentTabExempt = $exceptions.includes(currentTabHostname);
-        }
-
-        // remember the state and intent of the previous tab
-        if (previousTabId !== undefined && previousTabId !== tabId) {
-            tabState.set(previousTabId, { state, intent });
-        }
-
-        // restore the state and intent of the current tab if it exists
-        if (tabState.has(tabId)) {
-            ({ state, intent } = tabState.get(tabId));
-            tabState.delete(tabId);
-        } else {
-            state = defaultState;
-            intent = defaultIntent;
-
-            if (previousTabId !== undefined) {
-                tabState.set(previousTabId, { state, intent });
-            }
-        }
-
-        // remember the current tab as the previous tab id
         previousTabId = tabId;
+        previousTabHostname = currentTabHostname;
     }
+
 
     onMount(async () => {
         exceptions.subscribe(exceptionsChangedHandler);
 
         let [currentTab] = await browser.tabs.query({active: true, currentWindow: true});
 
+        currentTabId = currentTab.id;
+
         await tabChangedHandler(currentTab.id);
 
         browser.tabs.onActivated.addListener(event => tabChangedHandler(event.tabId));
         browser.tabs.onUpdated.addListener(event => tabChangedHandler(event));
         browser.tabs.onCreated.addListener(event => tabChangedHandler(event.id));
-
-        browser.tabs.onRemoved.addListener(event => tabState.delete(event.tabId));
     });
 </script>
 
 <Heading tag="h4">
-    {#if state === "PlayPause" }
-        {#if currentTabExempt}
-            Restart blocking on
-        {:else}
-            Pause blocking on
-        {/if}
-    {:else if state === "SendReport" }
-        Send AdReport for
-    {:else if state === "PreviewReport" }
-        Send
-    {:else if state === "Reload" }
-        Reload
-    {:else if state === "SendingReport" }
-        Uploading AdReport for
-    {/if}
-    <Secondary class="ms-2">{currentTabHostname}</Secondary>
-
-    {#if ["PlayPause", "SendReport", "PreviewReport", "Reload"].includes(state)}
-        ?
+    {#if currentState === "PlayPause" && !currentTabExempt }
+        Pause blocking on <Secondary class="ms-2">{currentTabHostname}</Secondary>?
+    {:else if currentState === "PlayPause" && currentTabExempt }
+        Restart blocking on <Secondary class="ms-2">{currentTabHostname}</Secondary>?
+    {:else if currentState.split("|")[0] === "PreviewReport" }
+        Send AdReport for <Secondary class="ms-2">{currentTabHostname}</Secondary>?
+    {:else if currentState === "SendingReport" }
+        Uploading AdReport for <Secondary class="ms-2">{currentTabHostname}</Secondary>
+    {:else if currentState === "Ask" }
+        Did it help?
     {/if}
 </Heading>
 
+<div>
+    {#if currentState === "PlayPause" && !currentTabExempt }
+        <p>In case you're experiencing problems (no access, popups or similar), try pausing to see if it helps!</p>
+    {:else if currentState === "PlayPause" && currentTabExempt }
+        <p>Blocking is paused. Click <i>Play</i> to restart blocking!</p>
+    {:else if currentState === "PreviewReport|ItHelped" }
+        <p>Great, let's keep the exception for now!</p>
+        <p>You can send a report! If you do we'll fix the problem for you and you'll be able to continue blocking on this page!</p>
+    {:else if currentState === "PreviewReport|DidntHelp" }
+        <p>Too bad, we've removed the exception again!</p>
+        <p>You can send a report! If you do we'll investigate and fix the problem for you!</p>
+    {:else if currentState === "Ask" }
+        <p>If it did (or if you want to disable blocking on this page) we'll keep this exception. Otherwhise we'll remove it again to make sure you remain protected on this page.</p>
+    {/if}
+</div>
+
 <div class="grid grid-cols-2 py-4 space-x-2">
-    {#if state === "PlayPause" }
+    {#if currentState === "PlayPause" && !currentTabExempt }
+
         <div class="text-right px-2">
-            <Button disabled={disabled} pill={true} outline={true} class="!p-2" on:click={clickPlayPause}>
-                {#if currentTabExempt}
-                    <PlaySolid class="w-10 h-10" />
-                {:else}
-                    <PauseSolid class="w-10 h-10" />
-                {/if}
+            <Button disabled={disabled} pill={true} outline={true} class="!p-2" on:click={clickPause}>
+                <PauseSolid class="w-10 h-10" />
             </Button>
         </div>
-    {:else if state === "SendReport" }
+    {:else if currentState === "PlayPause" && currentTabExempt }
+
         <div class="text-right px-2">
-            <Button disabled={disabled} pill={true} outline={true} class="!p-2"  on:click={clickPreview}>
+            <Button disabled={disabled} pill={true} outline={true} class="!p-2" on:click={clickPlay}>
+                <PlaySolid class="w-10 h-10" />
+            </Button>
+        </div>
+    {:else if currentState === "Ask" }
+        <div class="text-right px-2">
+            <Button disabled={disabled} pill={true} outline={true} class="!p-2"  on:click={clickItHelped}>
                 <CheckOutline class="w-10 h-10" />
             </Button>
         </div>
         <div class="px-2">
-            <Button disabled={disabled} pill={true} outline={true} class="!p-2"  on:click={clickNoAdReport}>
+            <Button disabled={disabled} pill={true} outline={true} class="!p-2"  on:click={clickDidntHelp}>
                 <CloseOutline class="w-10 h-10" />
             </Button>
         </div>
-    {:else if state === "PreviewReport" }
+    {:else if currentState.split("|")[0] === "PreviewReport" }
         <div class="text-right px-2">
             <Button disabled={disabled} pill={true} outline={true} class="!p-2"  on:click={clickSend}>
-                <ThumbsUpOutline class="w-10 h-10" />
+                <CheckOutline class="w-10 h-10" />
             </Button>
         </div>
         <div class="px-2">
             <Button disabled={disabled} pill={true} outline={true} class="!p-2"  on:click={clickNoSend}>
-                <ThumbsDownOutline class="w-10 h-10" />
-            </Button>
-        </div>
-    {:else if state === "SendingReport" }
-        <div class="text-right px-2">
-            <Spinner size={12} />
-        </div>
-    {:else if state === "Reload" }
-        <div class="text-right px-2">
-            <Button disabled={disabled} pill={true} outline={true} class="!p-2"  on:click={clickReload}>
-                <RefreshOutline class="w-10 h-10" />
-            </Button>
-        </div>
-        <div class="px-2">
-            <Button disabled={disabled} pill={true} outline={true} class="!p-2"  on:click={clickNoReload}>
                 <CloseOutline class="w-10 h-10" />
             </Button>
+        </div>
+    {:else if currentState === "SendingReport" }
+        <div class="text-right px-2">
+            <Spinner size={12} />
         </div>
     {/if}
 </div>
 
-{#if state === "PreviewReport" }
+{#if currentState.split("|")[0] === "PreviewReport" }
     {#if adReport.data}
         <AdReport {adReport} />
     {/if}
